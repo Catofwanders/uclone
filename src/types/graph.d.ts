7dd88dcc052300931e7991bef63a66c3effe19ac
Batura Taras <baturataras@gmail.com>
2020-06-30 07:39:05 +0300

    feat: created user entity

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,15 +1,33 @@
-export const typeDefs = ["type Greeting {\n  text: String!\n  error: Boolean!\n}\n\ntype Query {\n  user(name: String!): Greeting!\n}\n"];
+export const typeDefs = ["type User {\n  id: Int!\n  text: String!\n  email: Boolean!\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  createdAt: String!\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float!\n  lastLat: Float!\n  lastOrientation: Float\n}\n\ntype Query {\n  user(name: String!): User!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
-  user: Greeting;
+  user: User;
 }
 
 export interface UserQueryArgs {
   name: string;
 }
 
-export interface Greeting {
+export interface User {
+  id: number;
   text: string;
-  error: boolean;
+  email: boolean;
+  verifiedEmail: boolean;
+  firstName: string;
+  lastName: string;
+  age: number | null;
+  password: string | null;
+  phoneNumber: string | null;
+  verifiedPhoneNumber: boolean;
+  profilePhoto: string | null;
+  createdAt: string;
+  updatedAt: string | null;
+  fullName: string | null;
+  isDriving: boolean;
+  isRiding: boolean;
+  isTaken: boolean;
+  lastLng: number;
+  lastLat: number;
+  lastOrientation: number | null;
 }
