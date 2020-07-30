9faa6d4b092dd1f69ee4065fe694a43fd69ebd89
Batura Taras <baturataras@gmail.com>
2020-07-30 20:20:22 +0300

    feat: added Verification

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,12 +1,8 @@
-export const typeDefs = ["type User {\n  id: Int!\n  text: String!\n  email: Boolean!\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  createdAt: String!\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float!\n  lastLat: Float!\n  lastOrientation: Float\n}\n\ntype Query {\n  user(name: String!): User!\n}\n"];
+export const typeDefs = ["type User {\n  id: Int!\n  text: String!\n  email: Boolean!\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  createdAt: String!\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float!\n  lastLat: Float!\n  lastOrientation: Float\n}\n\ntype Query {\n  user: User!\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
   user: User;
-}
-
-export interface UserQueryArgs {
-  name: string;
 }
 
 export interface User {
@@ -30,4 +26,14 @@ export interface User {
   lastLng: number;
   lastLat: number;
   lastOrientation: number | null;
+}
+
+export interface Verification {
+  id: number;
+  target: string;
+  payload: string;
+  key: string;
+  used: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
